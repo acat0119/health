05303435028ae5981bf98a91ff269d754a0e5760
acat <1058927596@qq.com>
2019-08-11 16:36:32 +0800

    修改1

---
+++ gitTest/src/main/java/com/acat/pojo/User.java
@@ -0,0 +1,15 @@
+package com.acat.pojo;
+
+/**
+ * @author com.aCat
+ * @title: User
+ * @projectName health
+ * @description: TODO
+ * @date 2019/8/1116:33
+ */
+public class User {
+   private String name;
+   private String username;
+   private Integer age;
+
+}
